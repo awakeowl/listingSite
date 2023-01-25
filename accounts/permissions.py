21c6d542eed4f97d552c6c85141cf85c3f1c7999
brianmwevi <mwevibrian@gmail.com>
2023-01-25 11:02:19 +0300

    patch:add editor and viewer permissions check

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -7,5 +7,17 @@ class IsAdmin(permissions.BasePermission):
         return perform_check(request, "ADMIN")
 
 
+class IsEditor(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return perform_check(request, "EDITOR")
+
+
+class IsViewer(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return perform_check(request, "VIEWER")
+
+
 def perform_check(request, role):
     return request.user.role == role
